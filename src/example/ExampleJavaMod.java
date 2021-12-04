4bb1db3c25f85ae4f8983fa75af804cc00326ad2
VANIKO <92851776+VANIKO5265@users.noreply.github.com>
2021-12-04 16:16:02 +0300

    Update ExampleJavaMod.java

--- src/example/ExampleJavaMod.java
+++ src/example/ExampleJavaMod.java
@@ -1,6 +1,8 @@
 package example;
 
 import arc.*;
+import arc.scene.ui.Button;
+import arc.scene.ui.layout.Table;
 import arc.util.*;
 import arc.Events;
 import mindustry.*;
@@ -14,18 +16,11 @@ public class ExampleJavaMod extends Mod{
 
     public ExampleJavaMod() {
         Log.info("Loaded ExampleJavaMod constructor.");
-
-        //listen for game load event
-        Events.on(ClientLoadEvent.class, e -> {
-            BaseDialog dialog = new BaseDialog("Shit");
-            dialog.cont.add("behold").row();
-            //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-            dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
-            dialog.cont.button("I see", dialog::hide).size(100f, 50f);
-            dialog.show();
-            Log.info("ClientLoad Start correctly");
-        });
         Events.on(WorldLoadEvent.class, e -> {
+
+            Table but = new Table();
+            but.button("hi",null);
+
             BaseDialog dialog = new BaseDialog("Shit");
             dialog.cont.add("behold").row();
             dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
