f9f1a6c3688792e1c3b95e18efd48042b8d452ff
VANIKO <92851776+VANIKO5265@users.noreply.github.com>
2021-12-04 15:16:57 +0300

    Commit #1 (add WorldLoadEvent trigger)

--- src/example/ExampleJavaMod.java
+++ src/example/ExampleJavaMod.java
@@ -2,6 +2,7 @@ package example;
 
 import arc.*;
 import arc.util.*;
+import arc.Events;
 import mindustry.*;
 import mindustry.content.*;
 import mindustry.game.EventType.*;
@@ -11,20 +12,26 @@ import mindustry.ui.dialogs.*;
 
 public class ExampleJavaMod extends Mod{
 
-    public ExampleJavaMod(){
+    public ExampleJavaMod() {
         Log.info("Loaded ExampleJavaMod constructor.");
 
         //listen for game load event
         Events.on(ClientLoadEvent.class, e -> {
-            //show dialog upon startup
-            Time.runTask(10f, () -> {
-                BaseDialog dialog = new BaseDialog("frog");
-                dialog.cont.add("behold").row();
-                //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-                dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
-                dialog.cont.button("I see", dialog::hide).size(100f, 50f);
-                dialog.show();
-            });
+            BaseDialog dialog = new BaseDialog("Shit");
+            dialog.cont.add("behold").row();
+            //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
+            dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
+            dialog.cont.button("I see", dialog::hide).size(100f, 50f);
+            dialog.show();
+            Log.info("ClientLoad Start correctly");
+        });
+        Events.on(WorldLoadEvent.class, e -> {
+            BaseDialog dialog = new BaseDialog("Shit");
+            dialog.cont.add("behold").row();
+            dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
+            dialog.cont.button("I see", dialog::hide).size(100f, 50f);
+            dialog.show();
+            Log.info("WorldEvent Start correctly");
         });
     }
 
